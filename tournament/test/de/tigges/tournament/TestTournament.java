f386e4150fffbd7aafb8898fc1d7ce2d0eb2867e
johannes <johannes.tigges@freenet.de>
2015-01-31 15:25:50 +0100

    added checkTournament

--- tournament/test/de/tigges/tournament/TestTournament.java
+++ tournament/test/de/tigges/tournament/TestTournament.java
@@ -1,7 +1,16 @@
 package de.tigges.tournament;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import javafx.collections.ObservableList;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -13,6 +22,7 @@ import org.junit.Test;
 import de.tigges.tournament.logic.RandomUtil;
 import de.tigges.tournament.logic.RoundLogic;
 import de.tigges.tournament.logic.ScoreLogic;
+import de.tigges.tournament.model.Id;
 import de.tigges.tournament.model.Match;
 import de.tigges.tournament.model.Player;
 import de.tigges.tournament.model.Round;
@@ -69,7 +79,8 @@ public class TestTournament {
 	}
 	
 	private void printTournament(Tournament tournament) {
-		System.out.printf("Tournament (team size = %d, %d courts) with %d players and %d rounds%n", 
+		checkTournament(tournament);
+		System.out.printf("Tournament (%d team players, %d courts) with %d players and %d rounds%n", 
 				tournament.getTeamSize(), tournament.getCourts(), tournament.getPlayers().size(), tournament.getRounds().size());
 		for (Round round: tournament.getRounds()) {
 			System.out.printf("  Round %d with paused players %s%n", 
@@ -82,6 +93,72 @@ public class TestTournament {
 		}
 		for (Player player: tournament.getPlayers()) {
 			System.out.printf("Player %s has total score %d%n", player.getName(), player.getScore());
+		}
+	}
+	
+	private void checkTournament(Tournament tournament) {
+		assertNotNull(tournament);
+		assertNotNull(tournament.getTeamSize());
+		assertTrue(tournament.getTeamSize() > 0);
+		assertNotNull(tournament.getCourts());
+		assertTrue(tournament.getCourts() > 0);
+		assertNoDoubles(tournament.getPlayers());
+		for (Round round: tournament.getRounds()) {
+			checkRound(round, tournament);
+		}
+	}
+
+	private void checkRound(Round round, Tournament tournament) {
+		assertTrue(round.getPlayers().size() + round.getPausedPlayers().size() <= tournament.getPlayers().size());
+		assertTrue(round.getPlayers().size() % (tournament.getTeamSize() * 2) == 0);
+		assertTrue(round.getPausedPlayers().size() < (tournament.getTeamSize() * 2));
+		assertTrue(round.getPlayers().size() < tournament.getCourts() * tournament.getTeamSize() * 2);
+		assertTrue(round.getMatches().size() <= tournament.getCourts());
+		assertTrue(tournament.getPlayers().containsAll(round.getPlayers()));
+		assertTrue(tournament.getPlayers().containsAll(round.getPausedPlayers()));
+		assertDistinct(round.getPlayers(), round.getPausedPlayers());
+		assertNoDoubles(round.getPlayers());
+		assertNoDoubles(round.getPausedPlayers());
+		assertNoDoubles(round.getMatches());
+		
+		for (Match match: round.getMatches()) {
+			checkMatch(match,round,tournament);
+		}
+	}
+	
+
+	private void checkMatch(Match match, Round round, Tournament tournament) {
+		assertEquals(tournament.getTeamSize(), match.getHomeTeam().size());
+		assertEquals(tournament.getTeamSize(), match.getAwayTeam().size());
+		assertTrue(round.getPlayers().containsAll(match.getHomeTeam()));
+		assertTrue(round.getPlayers().containsAll(match.getAwayTeam()));
+		assertDistinct(match.getHomeTeam(), match.getAwayTeam());
+		assertNoDoubles(match.getHomeTeam());
+		assertNoDoubles(match.getAwayTeam());
+		for (Match otherMatch: round.getMatches()) {
+			if (otherMatch.equals(match) == false) {
+				assertDistinct(otherMatch.getHomeTeam(), match.getHomeTeam());
+				assertDistinct(otherMatch.getHomeTeam(), match.getAwayTeam());
+				assertDistinct(otherMatch.getAwayTeam(), match.getHomeTeam());
+				assertDistinct(otherMatch.getAwayTeam(), match.getAwayTeam());
+			}
+		}
+	}
+
+	private <I extends Id> void assertDistinct (ObservableList<I> l1, ObservableList<I> l2) {
+		for (I i1: l1) {
+			assertFalse(l2.contains(i1));
+		}
+		for (I i2: l2) {
+			assertFalse(l1.contains(i2));
+		}
+	}
+	
+	private <I extends Id> void assertNoDoubles(ObservableList<I> l) {
+		Set<I> set = new HashSet<I>();
+		for (I i: l) {
+			assertFalse(set.contains(i));
+			set.add(i);
 		}
 	}
 
